36395178736d67ad289f0395b9b9674244c165b4
Addster09 <aadityasingh.cs9@gmail.com>
2025-10-01 12:36:06 +0530

    Update mediatek hardware URL

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -5,10 +5,11 @@ git clone https://gitlab.com/mmtrt/device_xiaomi_everpal-kernel -b thirteen devi
 #sepolicy
 rm -rf device/mediatek/sepolicy_vndr && git clone https://github.com/MTK-DM-810-UNIFIED/android_device_mediatek_sepolicy_vndr -b lineage-20 device/mediatek/sepolicy_vndr
 #hw mtk
-rm -rf hardware/mediatek && git clone https://github.com/MTK-DM-810-UNIFIED/android_hardware_mediatek -b lineage-20-foss hardware/mediatek
+rm -rf hardware/mediatek && git clone https://github.com/PQEnablers-Devices/android_hardware_mediatek -b lineage-20-foss hardware/mediatek
 #hw ximi
 rm -rf hardware/xiaomi && git clone https://github.com/LineageOS/android_hardware_xiaomi -b lineage-20 hardware/xiaomi
 #miui cam
 git clone https://gitlab.com/mmtrt/vendor_xiaomi_camera-everpal.git -b thirteen vendor/xiaomi/camera
 #viperfx
 git clone https://github.com/Addster09/packages_apps_ViPER4AndroidFX packages/apps/ViPER4AndroidFX
+
