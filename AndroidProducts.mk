06561a4d38235de1894554ac353b956ac98db675
Addster09 <aadityasingh.cs9@gmail.com>
2025-11-10 23:39:50 +0530

    Cleanup!

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -7,16 +7,12 @@
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/arrow_everpal.mk \
     $(LOCAL_DIR)/derp_everpal.mk \
-    $(LOCAL_DIR)/spark_everpal.mk \
     $(LOCAL_DIR)/lineage_everpal.mk
 
 COMMON_LUNCH_CHOICES := \
     derp_everpal-user \
     derp_everpal-userdebug \
     derp_everpal-eng\
-    spark_everpal-user \
-    spark_everpal-userdebug \
-    spark_everpal-eng\
     arrow_everpal-user \
     arrow_everpal-userdebug \
     arrow_everpal-eng \
