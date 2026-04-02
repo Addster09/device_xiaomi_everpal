79b7553fbba1fa43afb614103e7657eec463551c
Addster09 <aadityasingh.cs9@hotmail.com>
2026-04-02 18:43:01 +0530

    everpal: Update memtrack service module name
    The memtrack module in hardware/mediatek dropped the '-mali' suffix
    and is now named android.hardware.memtrack-service.mediatek. Updated 
    PRODUCT_PACKAGES to match the new AIDL implementation.
    
    Signed-off-by: Addster09 <aadityasingh.cs9@gmail.com>

--- device.mk
+++ device.mk
@@ -154,7 +154,7 @@ PRODUCT_PACKAGES += \
 # Graphics
 PRODUCT_PACKAGES += \
     android.hardware.graphics.composer@2.3-service \
-    android.hardware.memtrack-service.mediatek-mali
+    android.hardware.memtrack-service.mediatek
 
 # Health
 PRODUCT_PACKAGES += \
