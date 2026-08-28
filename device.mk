331a18b596942c220055cb979650bf54190f7954
Addster09 <aadityasingh.cs9@gmail.com>
2026-08-28 14:13:28 +0530

    everpal: Switch to lineage libperfmgr
    Signed-off-by: Addster09 <aadityasingh.cs9@gmail.com>

--- device.mk
+++ device.mk
@@ -286,7 +286,7 @@ PRODUCT_COPY_FILES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power-service.pixel-libperfmgr \
+    android.hardware.power-service.lineage-libperfmgr \
     vendor.mediatek.hardware.mtkpower@1.2-service.stub \
     libmtkperf_client_vendor
 
@@ -329,6 +329,7 @@ PRODUCT_SOONG_NAMESPACES += \
     hardware/google/interfaces \
     hardware/google/pixel \
     hardware/mediatek \
+    hardware/lineage/interfaces/power-libperfmgr \
     hardware/mediatek/libmtkperf_client \
     hardware/xiaomi
 
